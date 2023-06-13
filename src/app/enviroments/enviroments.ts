dc0aef87ab7fe9ddd4a60758109e09c4f251e61a
italolouis <italolouis@users.noreply.github.com>
2023-06-12 21:34:00 -0300

    Alterações

--- src/app/enviroments/enviroments.ts
+++ src/app/enviroments/enviroments.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   url_dev:  'http://localhost:8084',
   url_prod: 'https://sgfly-api-production.up.railway.app',
-  url_server: 'http://localhost:8084',
+  url_server: 'https://sgfly-api-production.up.railway.app',
 };
